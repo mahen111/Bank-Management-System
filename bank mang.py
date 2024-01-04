a56107f78e55bbfb51d659942babe1c3c99f890a
mahen111 <152676258+mahen111@users.noreply.github.com>
2024-01-04 12:36:41 +0530

    Insurance Bank

---
+++ bank mang.py
@@ -0,0 +1,22 @@
+
+class Employee:
+
+# property
+    name = "vikas ravidas"
+    age = 21
+    address = "bhopal"
+    designation = "Developer"
+    organization = "google"
+
+def display_data(self):
+    # self is the keyword within is used to the current existance
+
+    print(self.name, self.age, self.ageorganization, self.address)
+    # self is key word
+
+    # making object for tje class
+    # object name             class name
+    #employer          =        employee
+
+employer=Employee()
+Employee.display.data()
